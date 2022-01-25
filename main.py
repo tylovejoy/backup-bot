545ab6cdbf82d64853a335c97a6d7833da1f6627
Ty <ty@fromskytocenter.com>
2022-01-24 19:20:40 -0600

    requirements.txt, added logging

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import logging
+from os import environ
+
 import discord
 from discord.ext import commands
 
@@ -9,10 +12,20 @@ intents.presences = False
 bot = commands.Bot(command_prefix='?', description="", intents=intents)
 
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+consoleHandle = logging.StreamHandler()
+consoleHandle.setLevel(logging.INFO)
+consoleHandle.setFormatter(
+    logging.Formatter("%(name)-18s :: %(levelname)-8s :: %(message)s")
+)
+logger.addHandler(consoleHandle)
+
+
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
-    print('------')
+    logger.info(f"Logged in as {bot.user} (ID: {bot.user.id})\n\n")
 
 
 @bot.event
@@ -27,25 +40,25 @@ async def on_message(message):
 @bot.command()
 async def backup(ctx: discord.ext.commands.Context):
     for channel in ctx.guild.channels:
-        print(channel.name, channel.id)
+
+        # Ignore non text channels
         if channel.type != discord.ChannelType.text:
             continue
 
         messages = await channel.history(limit=None, oldest_first=True).flatten()
         documents = []
+
         for message in messages:
             documents.append(
                 Message(
                     name=message.name,
                     content=message.content,
                     timestamp=message.created_at,
-
                 )
             )
-
         await Message.insert_many(documents)
 
-
-bot.run('ODc4ODgyMzY0NjExMjYwNDU2.YSHo_A.30V4Et8fURCjBDOjTzZ4-7Pxj7w')
+TOKEN = environ["TOKEN"]
+bot.run(TOKEN)
 
 
